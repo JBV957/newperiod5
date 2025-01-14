85331614496e83e3a42b6fe50b477daa8b27cf79
JBV957 <josuev25@nycstudents.net>
2025-01-14 13:36:54 -0500

    Update script.js
    Explaining

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const clickCountDisplay = document.getElementById('click-count');
         const header = document.getElementById('header');
         const button = document.getElementById('click-button');
        
-//1.Add a comment here explaining the let clickCount variable  
+//1.The clickCount variable is here to store the amount of clicks is inisalized and also this is where the variable clickCount is made
         let clickCount = localStorage.getItem('clickCount') ? parseInt(localStorage.getItem('clickCount')) : 0;
 
         // Update display on load
         clickCountDisplay.textContent = clickCount;
         updateUI();
 
-//2.Explain what this event listener function does (and what function it calls).
+//2.The listener function add from clickcrount to add on to the storage.
         // Add event listener to button
         button.addEventListener('click', () => {
             clickCount++;
@@ -18,7 +18,7 @@ const clickCountDisplay = document.getElementById('click-count');
             updateUI();
         });
 
-//3.Explain what function updateUI does and is holding the colors and texts
+//3.The function updateUI changes colors on how fast you click per secound and has diffrent sentences on how are you progressing.
         function updateUI() {
             const colors = ['#FF5733', '#33FF57', '#3357FF', '#F5A623', '#E91E63'];
             const texts = [
